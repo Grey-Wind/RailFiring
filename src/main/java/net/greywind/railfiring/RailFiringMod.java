16fb33f80b69acb314a2431f71281b1454a9bb9b
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-01-29 23:37:08 +0800

    update info

--- src/main/java/net/greywind/railfiring/RailFiringMod.java
+++ src/main/java/net/greywind/railfiring/RailFiringMod.java
@@ -11,7 +11,7 @@
  *    will be REGENERATED on each build.
  *
  */
-package net.qingyi.railfiring;
+package net.greywind.railfiring;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
