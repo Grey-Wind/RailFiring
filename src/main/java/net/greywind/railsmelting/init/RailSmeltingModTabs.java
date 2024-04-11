e3a8015609aa4d579520309d3acb8cd3fdf7c934
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-04-11 16:15:53 +0800

    creative tab

--- src/main/java/net/greywind/railsmelting/init/RailSmeltingModTabs.java
+++ src/main/java/net/greywind/railsmelting/init/RailSmeltingModTabs.java
@@ -5,24 +5,22 @@
 package net.greywind.railsmelting.init;
 
 import net.neoforged.neoforge.registries.DeferredRegister;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.registries.DeferredHolder;
 
-import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.network.chat.Component;
 import net.minecraft.core.registries.Registries;
 
 import net.greywind.railsmelting.RailSmeltingMod;
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class RailSmeltingModTabs {
 	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, RailSmeltingMod.MODID);
+	public static final DeferredHolder<CreativeModeTab, CreativeModeTab> RAIL_SMELTING_CREATIVE_TAB = REGISTRY.register("rail_smelting_creative_tab",
+			() -> CreativeModeTab.builder().title(Component.translatable("item_group.rail_smelting.rail_smelting_creative_tab")).icon(() -> new ItemStack(Blocks.RAIL)).displayItems((parameters, tabData) -> {
+				tabData.accept(RailSmeltingModBlocks.RAIL_FURNACE.get().asItem());
+			})
 
-	@SubscribeEvent
-	public static void buildTabContentsVanilla(BuildCreativeModeTabContentsEvent tabData) {
-		if (tabData.getTabKey() == CreativeModeTabs.INGREDIENTS) {
-			tabData.accept(RailSmeltingModBlocks.RAIL_FURNACE.get().asItem());
-		}
-	}
+					.build());
 }
