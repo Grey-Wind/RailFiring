86928d7dbcefa6a34aac95275da0bb56d14243a5
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2024-04-11 16:25:48 +0800

    buy rail

--- src/main/java/net/greywind/railsmelting/init/RailSmeltingModTrades.java
+++ src/main/java/net/greywind/railsmelting/init/RailSmeltingModTrades.java
@@ -19,18 +19,32 @@ public class RailSmeltingModTrades {
 	@SubscribeEvent
 	public static void registerTrades(VillagerTradesEvent event) {
 		if (event.getType() == VillagerProfession.TOOLSMITH) {
-			event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Blocks.RAIL, 20),
+			event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Blocks.RAIL, 15),
 
 					new ItemStack(Items.EMERALD), 10, 5, 0.05f));
 			event.getTrades().get(3).add(new BasicItemListing(new ItemStack(Blocks.POWERED_RAIL, 7),
 
 					new ItemStack(Items.EMERALD), 10, 5, 0.05f));
-			event.getTrades().get(2).add(new BasicItemListing(new ItemStack(Blocks.DETECTOR_RAIL),
+			event.getTrades().get(2).add(new BasicItemListing(new ItemStack(Blocks.DETECTOR_RAIL, 10),
 
 					new ItemStack(Items.EMERALD), 10, 5, 0.05f));
-			event.getTrades().get(2).add(new BasicItemListing(new ItemStack(Blocks.ACTIVATOR_RAIL),
+			event.getTrades().get(2).add(new BasicItemListing(new ItemStack(Blocks.ACTIVATOR_RAIL, 10),
 
 					new ItemStack(Items.EMERALD), 10, 5, 0.05f));
+		}
+		if (event.getType() == VillagerProfession.TOOLSMITH) {
+			event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 5),
+
+					new ItemStack(Blocks.RAIL), 10, 5, 0.05f));
+			event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 10),
+
+					new ItemStack(Blocks.POWERED_RAIL), 10, 5, 0.05f));
+			event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 5),
+
+					new ItemStack(Blocks.DETECTOR_RAIL), 10, 5, 0.05f));
+			event.getTrades().get(1).add(new BasicItemListing(new ItemStack(Items.EMERALD, 5),
+
+					new ItemStack(Blocks.ACTIVATOR_RAIL), 10, 5, 0.05f));
 		}
 	}
 }
